b9a98608690cf995ebbd9d1cc6329ac874d0ee65
Jesse Chen <jesse.z.chen@gmail.com>
2020-03-26 17:05:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Jumbletron
+
+### Jumbles words, leaving the first and last letters in the right place.
+
+Production
+----------
+
+TBD
+
+Inspiration
+-----------
+
+"Aoccdrnig to a rscheearch at Cmabrigde Uinervtisy, it deosn't mttaer in waht oredr the ltteers in a wrod are, the olny iprmoetnt tihng is taht the frist and lsat ltteer be at the rghit pclae."
+
+https://www.mrc-cbu.cam.ac.uk/people/matt.davis/cmabridge/
+
+
+License
+-------
+
+MIT
---
+++ jumbletron.js
@@ -0,0 +1,35 @@
+function jumbletron() {
+    const textElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, li, td, caption, pre, span, a');
+
+    for (let textElement of textElements) {
+        const result = [];
+        const words = textElement.innerText.trim().split(' ');
+        for (let word of words) {
+            result.push(jumble(word));
+        }
+        textElement.innerHTML = result.join(' ');
+    }
+}
+
+function jumble(word) {
+    if (word.length <= 3) {
+        return word;
+    }
+
+    let swap = '';
+    let trailingChar = '';
+    const chars = word.split('');
+    if ([',', '.', ';', ':'].includes(chars[chars.length - 1])) {
+        trailingChar = chars[chars.length - 1];
+        chars.length -= 1;
+    }
+    for (let i = 1; i <= chars.length - 2; i++) {
+        const swapIndex = Math.floor(Math.random() * (chars.length - i - 1) + i);
+        swap = chars[swapIndex];
+        chars[swapIndex] = chars[i];
+        chars[i] = swap;
+    }
+    return chars.join('') + trailingChar;
+}
+
+jumbletron();
